09acbbd179112ff3a10c165b26b490404f35f025
Ranji <Ranji@Aanie>
2025-05-30 12:19:16 -0400

    Commiting 6 files

---
+++ Revision/src/main/java/org/iitworkforce/pixel/Revision/SimpleProgram.java
@@ -0,0 +1,16 @@
+package org.iitworkforce.pixel.Revision;
+
+
+		public class SimpleProgram {
+		    public static void main(String[] args)
+		    {
+		        System.out.println("Hello, World!");
+		        System.out.println("Hello, World!");
+		        System.out.println("Hello, World!");
+		        System.out.println("Hello, World!");
+		    }
+		
+
+	}
+
+
---
+++ Revision/src/test/java/org/iitworkforce/pixel/Revision/FullScreenShot.java
@@ -0,0 +1,5 @@
+package org.iitworkforce.pixel.Revision;
+
+public class FullScreenShot {
+
+}
